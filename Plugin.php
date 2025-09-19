d1cf49f7d7c855649d6239409dd35493fe12fecc
Coder <temp@no.reply.com>
2025-09-19 14:26:15 +0300

    Typography fixes and mobile pretty up design

--- Plugin.php
+++ Plugin.php
@@ -65,6 +65,12 @@ class Plugin extends Base
 
 		// main js
 		$this->hook->on('template:layout:js', array('template' => 'plugins/ThemeRevision/Asset/main.min.js'));
+		
+		// mobile fixes css
+		$this->hook->on('template:layout:css', array('template' => 'plugins/ThemeRevision/Asset/dev/css/mobile-fixes.css'));
+		
+		// typography css
+		$this->hook->on('template:layout:css', array('template' => 'plugins/ThemeRevision/Asset/dev/css/typography.css'));
 	}
 
 	public function onStartup(){
--- Template/layout.php
+++ Template/layout.php
@@ -2,8 +2,13 @@
 <html lang="<?= $this->app->jsLang() ?>">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
         <meta name="mobile-web-app-capable" content="yes">
+        
+        <!-- Inter font from Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
         <meta name="robots" content="noindex,nofollow">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="referrer" content="no-referrer">
